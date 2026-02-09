cfe7e31eeea5268e67eb4b0ef0cf889347eff279
Andy Connacher <48026026+theflyingepergne@users.noreply.github.com>
2026-02-09 14:49:09 +0000

    if we click, print what we hit

--- Assets/Scripts/ClickScript.cs
+++ Assets/Scripts/ClickScript.cs
@@ -4,18 +4,33 @@ using UnityEngine.InputSystem;
 public class ClickScript : MonoBehaviour
 {
     InputAction clickAction;
-    GameObject selectedObject;
 
-    public void Start()
+    void Start()
     {
         clickAction = InputSystem.actions.FindAction("Click");
     }
-        
+
+    void Update()
+    {
+        // Get mouse position in screen coordinates
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+
+        // Convert mouse position to world coordinates and perform a raycast
+        RaycastHit2D hit2D = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(mousePos), Vector2.zero);
+
+        // If we released the click action this frame, check if we hit something
+        if (clickAction.WasReleasedThisFrame())
+        {
+            if ( hit2D.collider != null)
+            {
+                Debug.Log("Mouse Clicked: " + hit2D.collider.gameObject.name + " at position: " + mousePos);    
+            }
+            else
+            {
+                Debug.Log("Hit nothing at: " + mousePos);
+            }
+            
+        }
+    }
 }
 
-public void Update()
-{
-    
-}
-
-    
