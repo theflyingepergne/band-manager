9057c293310bc6c51c8c8dbf28f30cad092eac4f
Andy Connacher <48026026+theflyingepergne@users.noreply.github.com>
2026-02-12 14:23:27 +0000

    added setlist

--- Assets/Scripts/PerformGigUIManager.cs
+++ Assets/Scripts/PerformGigUIManager.cs
@@ -4,6 +4,20 @@ using UnityEngine.UIElements;
 public class PerformGigUIManager : Singleton<PerformGigUIManager>
 {
     [SerializeField] private UIDocument performGigUI;
-    
 
+    private VisualElement root;
+    private VisualElement setlistUI;
+
+    public void Start()
+    {
+        root = performGigUI.rootVisualElement;
+        setlistUI = root.Q<ScrollView>("Setlist");
+        
+        for (int i = 0; i < 5; i++)
+        {
+            Label songLabel = new Label();
+            songLabel.text = (i + 1) + ". Song name " + (i + 1); // Eventually this will be actual songs written by 
+            setlistUI.Add(songLabel);
+        }
+    }
 }
