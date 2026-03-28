8b1dbefd0ea19c4980483815e05eab914be7d5cc
Andy Connacher <48026026+theflyingepergne@users.noreply.github.com>
2026-03-28 12:28:48 +0000

    changed main menu to use old ui

--- Assets/Scripts/MainMenuUIManager.cs
+++ Assets/Scripts/MainMenuUIManager.cs
@@ -1,41 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
 public class MainMenuUIManager : Singleton<MainMenuUIManager>
 {
-    [SerializeField] private UIDocument mainMenuUI;
-    private Button playButton;
-    private Button settingsButton;
-    private Button quitButton;
-
-    private void Start()
-    {
-        var root = mainMenuUI.rootVisualElement;
-
-        // Hooking up buttons using strings...
-        playButton = mainMenuUI.rootVisualElement.Q<Button>("PlayButton");
-        settingsButton = mainMenuUI.rootVisualElement.Q<Button>("SettingsButton");
-        quitButton = mainMenuUI.rootVisualElement.Q<Button>("QuitButton");
-
-        playButton.clicked += OnPlayButtonClicked;
-        settingsButton.clicked += OnSettingsButtonClicked;
-        quitButton.clicked += OnQuitButtonClicked;
-    }
-
-    private void OnPlayButtonClicked()
+    public void OnPlayButtonClicked()
     {
         // Eventually play actual game but for now load ViewBandMember scene
         SceneManager.LoadScene("ViewBandMember");
     }
 
-    private void OnSettingsButtonClicked()
+    public void OnSettingsButtonClicked()
     {
         // Implement settings functionality here
         Debug.Log("Settings button clicked!");
     }
 
-    private void OnQuitButtonClicked()
+    public void OnQuitButtonClicked()
     {
         Application.Quit();
     }
