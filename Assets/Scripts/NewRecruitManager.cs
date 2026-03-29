77918d5a80f1914774294bec15e7a3084c5f7efa
Andy Connacher <48026026+theflyingepergne@users.noreply.github.com>
2026-03-29 18:37:54 +0100

    recruit dismiss buttons are now toggleable

--- Assets/Scripts/NewRecruitManager.cs
+++ Assets/Scripts/NewRecruitManager.cs
@@ -1,16 +1,54 @@
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class NewRecruitManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Data")]
+    [SerializeField] private BandMemberData data;
+
+    [Header("UI References")]
+    [SerializeField] private Image recruitSprite;
+    [SerializeField] private TMP_Text recruitName;
+    [SerializeField] private TMP_Text recruiteInstruments;
+    [SerializeField] private TMP_Text recruitTraits;
+    [SerializeField] private RectTransform recruitDismissPanel;
+
+    public void Start()
     {
+        recruitSprite.sprite = data.memberSprite;
+        recruitName.text = data.memberName;
+        recruiteInstruments.text = $"Plays {data.instruments[0].instrumentName}";
+        recruitTraits.text = $"Seems {data.traits[0]}...";
         
+        recruitDismissPanel.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleRecruitDismissPanel()
     {
-        
+        if (recruitDismissPanel.gameObject.activeSelf == true)
+        {
+            SetRecruitDismissPanelVisibility(false);
+            Debug.Log("hiding options");
+        }
+        else
+        {
+            SetRecruitDismissPanelVisibility(true);
+            Debug.Log("showing options");
+        }
+    }
+
+    public void SetRecruitDismissPanelVisibility(bool visible)
+    {
+        recruitDismissPanel.gameObject.SetActive(visible);
+    }
+
+    public void Recruit()
+    {
+        Debug.Log("Recruited");
+    }
+
+    public void Dismiss()
+    {
+        Debug.Log("Dismissed");
     }
 }
