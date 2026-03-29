64cbba9df0a5f5532040eedf154e20f9ec921bfb
Andy Connacher <48026026+theflyingepergne@users.noreply.github.com>
2026-03-29 19:47:16 +0100

    set up my first ever event to toggle recruit dismiss panel for other open panels and recorded demo

--- Assets/Scripts/NewRecruitManager.cs
+++ Assets/Scripts/NewRecruitManager.cs
@@ -1,8 +1,10 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class NewRecruitManager : MonoBehaviour
 {
+    //---References---//
     [Header("Data")]
     [SerializeField] private BandMemberData data;
 
@@ -13,31 +15,41 @@ public class NewRecruitManager : MonoBehaviour
     [SerializeField] private TMP_Text recruitTraits;
     [SerializeField] private RectTransform recruitDismissPanel;
 
-    public void Start()
+    //---Events---//
+    // Close other recruit panel options if one is opened
+    public static Action<NewRecruitManager> OnAnyPanelOpened;
+    void OnEnable() => OnAnyPanelOpened += CloseIfNotThis;
+    void OnDisable() => OnAnyPanelOpened -= CloseIfNotThis;
+
+    //---Methods---//
+
+    void Start()
     {
         recruitSprite.sprite = data.memberSprite;
         recruitName.text = data.memberName;
         recruiteInstruments.text = $"Plays {data.instruments[0].instrumentName}";
         recruitTraits.text = $"Seems {data.traits[0]}...";
-        
-        recruitDismissPanel.gameObject.SetActive(false);
+
+        SetRecruitDismissPanelVisibile(false);
     }
 
     public void ToggleRecruitDismissPanel()
     {
+        OnAnyPanelOpened?.Invoke(this);
+
         if (recruitDismissPanel.gameObject.activeSelf == true)
         {
-            SetRecruitDismissPanelVisibility(false);
+            SetRecruitDismissPanelVisibile(false);
             Debug.Log("hiding options");
         }
         else
         {
-            SetRecruitDismissPanelVisibility(true);
+            SetRecruitDismissPanelVisibile(true);
             Debug.Log("showing options");
         }
     }
 
-    public void SetRecruitDismissPanelVisibility(bool visible)
+    private void SetRecruitDismissPanelVisibile(bool visible)
     {
         recruitDismissPanel.gameObject.SetActive(visible);
     }
@@ -50,5 +62,13 @@ public class NewRecruitManager : MonoBehaviour
     public void Dismiss()
     {
         Debug.Log("Dismissed");
+    }
+
+    private void CloseIfNotThis(NewRecruitManager openedManager)
+    {
+        if (openedManager != this)
+        {
+            SetRecruitDismissPanelVisibile(false);
+        }
     }
 }
