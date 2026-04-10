b70731a06793f68af1f94871cd1c2a3dcd0febd9
Andy Connacher <48026026+theflyingepergne@users.noreply.github.com>
2026-04-10 21:10:35 +0100

    made TestScript to quickly generate songs for testing

---
+++ Assets/Scripts/Tests/TestScript.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestSongGenerator : MonoBehaviour
+{
+    [SerializeField] private BandMemberData bandMemberData;
+    void Update()
+    {
+        if (Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            GenerateTestSongs();
+        }
+    }
+
+    void GenerateTestSongs()
+    {
+        // Clear existing to avoid duplicates if you press T twice
+        BandManager.Instance.songCollection.Clear();
+
+        string[] titles =
+        {
+            "Bleed American",
+            "The Middle",
+            "Sweetness",
+            "Hear You Me",
+            "Authority Song",
+            "My Best Theory",
+            "A Praise Chorus",
+            "Lucky Denver Mint",
+            "Work",
+            "Always Be"
+        };
+
+        for (int i = 0; i < titles.Length; i++)
+        {
+            float newSongScore = UnityEngine.Random.Range(0f, 100f);
+            SongEntry newSong = new SongEntry(titles[i], bandMemberData, newSongScore);
+            BandManager.Instance.AddSongToCollection(newSong);
+        }
+
+        Debug.Log($"Generated {BandManager.Instance.songCollection.Count} test songs in BandManager.");
+    }
+}
+
