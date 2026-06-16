73246670b07f37851fc57545be1bfd219493d039
Andy Connacher <48026026+theflyingepergne@users.noreply.github.com>
2026-06-17 00:16:14 +0100

    init ink plugin

---
+++ Assets/Ink/Demos/Basic Demo/Scripts/QuitGameOnKeypress.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class QuitGameOnKeypress : MonoBehaviour
+{		
+	void Update()
+	{
+		if (Keyboard.current.escapeKey.wasPressedThisFrame)
+		{
+			Application.Quit();
+		}
+	}
+}
