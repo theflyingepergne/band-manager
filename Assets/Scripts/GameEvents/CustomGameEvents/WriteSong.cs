fcb016f29bbf26d6d9b45f86dd742e59e82d3dab
Andy Connacher <48026026+theflyingepergne@users.noreply.github.com>
2026-07-21 16:04:42 +0100

    started on WriteSong custom game event
    - need to implement variables passing through to actual outputted song

--- Assets/Scripts/GameEvents/CustomGameEvents/WriteSong.cs
+++ Assets/Scripts/GameEvents/CustomGameEvents/WriteSong.cs
@@ -3,6 +3,22 @@ public class WriteSong : CustomGameEvent
 {
     public override void Execute()
     {
-        
+        // setup song details
+        string songName = "test";
+        BandMemberInstance bandMember = null;
+        GenreWeights genreWeights = null;
+        float newSongScore = 0f;
+
+        // create newSong
+        SongEntry newSong = new
+        (
+            songName,
+            bandMember,
+            genreWeights,
+            newSongScore
+        );
+
+        // add song to collection
+        BandManager.Instance.AddSongToCollection(newSong);
     }
 }
