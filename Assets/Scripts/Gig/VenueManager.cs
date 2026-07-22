92f2a3801868c31778c7c30153f9d82afe429421
Andy Connacher <48026026+theflyingepergne@users.noreply.github.com>
2026-07-22 22:54:55 +0100

    created "Shop" scene, venues now change background
    - need to add the ability to change dialogue and venue owner stuff based on venueData

--- Assets/Scripts/Gig/VenueManager.cs
+++ Assets/Scripts/Gig/VenueManager.cs
@@ -1,12 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VenueManager : Singleton<VenueManager>
 {
     //---References---//
+    [Header("UI")]
     [SerializeField] private DialogueManager dialogueManager;
+    [SerializeField] private Image venueBackground;
+    public VenueData venueData;
+
+    //---Local References---//
 
     private void OnEnable() => CameraFade.OnFadeInComplete += HandleFadeInComplete;
     private void OnDisable() => CameraFade.OnFadeInComplete -= HandleFadeInComplete;
+
+    private void Start()
+    {
+        if (BandManager.Instance.destinationVenue != null)
+        {
+            venueData = BandManager.Instance.destinationVenue;
+            venueBackground.sprite = venueData.backgroundSprite;
+        }
+    }
 
     // After fade in, should we: start gig, talk to venue owner or watch another band play?
     private void HandleFadeInComplete()
